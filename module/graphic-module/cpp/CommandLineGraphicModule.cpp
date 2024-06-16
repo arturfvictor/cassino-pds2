966ee55ee1ad56ba447962b91ec987f02aa5302a
arturfvictor <arturfvictor22@gmail.com>
2024-06-16 14:59:55 -0300

    add println to command line graphic module

--- module/graphic-module/cpp/CommandLineGraphicModule.cpp
+++ module/graphic-module/cpp/CommandLineGraphicModule.cpp
@@ -29,6 +29,19 @@ void CommandLineGraphicModule::print(string text, int speed, bool bold, bool und
     disableTextFormatting(bold, underline);
 }
 
+void CommandLineGraphicModule::println(string text, int speed, bool bold, bool underline) { //speed == chars/second
+    text += "\n";
+    enableTextFormatting(bold, underline);
+
+    int waitTime = 1000 / speed; 
+    for (int i = 0; i < text.size(); i++) {
+        cout << text[i] << flush;
+        sleep_for(milliseconds(waitTime));
+    }
+
+    disableTextFormatting(bold, underline);
+}
+
 void CommandLineGraphicModule::clear() {
     #ifdef _WIN32
     std::system("cls");
