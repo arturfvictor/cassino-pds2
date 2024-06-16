c5a6902e2a84d6abaa392eebde13056ddade6d30
arturfvictor <arturfvictor22@gmail.com>
2024-06-16 16:58:42 -0300

    add comments explaining every module method

--- module/graphic-module/hpp/GraphicModule.hpp
+++ module/graphic-module/hpp/GraphicModule.hpp
@@ -7,8 +7,22 @@ using std::string;
 
 class GraphicModule {
     public:
-        void virtual print(string text, int speed, bool bold, bool underline) = 0; // speend == chars/second
+        /// @brief Imprime texto em tela
+        /// @param text Texo a ser impresso
+        /// @param speed Velocidade em caracteres por segundo em que o texto deve ser impresso
+        /// @param bold Se o texto deve ser impresso em negrito
+        /// @param underline Se o texto deve ser impresso sublinhado
+        void virtual print(string text, int speed, bool bold, bool underline) = 0;
+
+        /// @brief Imprime texto em tela com uma quebra de linha no final
+        /// @param text Texo a ser impresso
+        /// @param speed Velocidade em caracteres por segundo em que o texto deve ser impresso
+        /// @param bold Se o texto deve ser impresso em negrito
+        /// @param underline Se o texto deve ser impresso sublinhado
         void virtual println(string text, int speed, bool bold, bool underline) = 0;
+
+
+        /// @brief Limpa a tela
         void virtual clear() = 0;
 };
 
